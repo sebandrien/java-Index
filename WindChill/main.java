e859d9ed3c9a95a6e0a47dcb43ef6d29a234b647
sebandrien <129107648+sebandrien@users.noreply.github.com>
2024-02-16 15:54:15 -0500

    Update main.java

--- WindChill/main.java
+++ WindChill/main.java
@@ -1,43 +1,31 @@
-// WindChill GUI
-// Developed: June 2007
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class WindChill implements ActionListener {
-
-    // Class constants
+    
     private static final int WINDOW_WIDTH = 410;       
     private static final int WINDOW_HEIGHT = 235;      
     private static final int FIELD_WIDTH = 20;       
     private static final int AREA_WIDTH = 40;         
     private static final FlowLayout LAYOUT_STYLE = new FlowLayout();
     private static final String LEGEND = "This windchill calculator is intended for wind speeds greater than 4 mph.";
-
-    // Instance variables
-    // Window for GUI
+    
     private JFrame window = new JFrame("Windchill Calculator");
 
-    // Legend
     private JTextArea legendArea = new JTextArea(LEGEND, 2, AREA_WIDTH);
 
-    // User entry area for temperature
     private JLabel fahrTag = new JLabel("Fahrenheit Temperature");
     private JTextField fahrText = new JTextField(FIELD_WIDTH);
 
-    // User entry area for windspeed
     private JLabel windTag = new JLabel("Windspeed (mph)");
     private JTextField windText = new JTextField(FIELD_WIDTH);
 
-    // Entry area for windchill result
     private JLabel chillTag = new JLabel("Windchill Temperature");
     private JTextField chillText = new JTextField(FIELD_WIDTH);
 
-    // Run button
     private JButton runButton = new JButton("Run");
 
-    // WindChill(): Constructor
     public WindChill() {
         configureGUI();
     }
@@ -71,8 +59,7 @@ public class WindChill implements ActionListener {
 
         window.setVisible(true);
     }
-
-    // actionPerformed(): run button action event handler
+    
     public void actionPerformed(ActionEvent e) {
         double t = Double.parseDouble(fahrText.getText());
         double v = Double.parseDouble(windText.getText());
@@ -83,7 +70,6 @@ public class WindChill implements ActionListener {
         chillText.setText(String.valueOf(perceivedTemperature));
     }
 
-    // main(): application entry point
     public static void main(String[] args) {
         new WindChill();
     }
